26043b44e34681919cc03c99fbdfec02cddd3273
Raiix <imbosslx@sina.com>
2020-04-17 01:27:14 +0800

    Add readme

--- compile.bat
+++ compile.bat
@@ -4,6 +4,7 @@ set include=%masm_dir%\Include
 set lib=%masm_dir%\lib
 set path=%masm_dir%\Bin;%masm_dir%;%path%
 
+rem 由于需要用到vc++的nmake程序来对项目进行构建，所以请设定好VsDevCmd的路径
 set vc_dev_bat_path="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 call %vc_dev_bat_path%
 
